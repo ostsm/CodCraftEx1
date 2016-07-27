5009b5b5c0617f0b340e63b21f6f2f507f43ce7f
Lucas <Lucas@LUCAS-NOT>
2016-07-26 21:28:06 -0300

    Entradas relatório

---
+++ CodingCraftHOMod1Ex1EF/Controllers/RelatoriosController.cs
@@ -0,0 +1,53 @@
+﻿using CodingCraftHOMod1Ex1EF.ViewModel;
+using IdentitySample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodingCraftHOMod1Ex1EF.Controllers
+{
+    public class RelatoriosController : Controller
+    {
+        // GET: Relatorios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Entradas()
+        {
+
+            using (var db = new ApplicationDbContext())
+            {
+                var dados = from ft in db.MovimentacaoFornecedors
+                            orderby ft.Data
+                            select new MovimentacaoFornecedorsPesquisaModel()
+                            {
+                                Data = ft.Data,
+                              //  FormaPagamento = ft.FormaPagamento,
+                                FornecedorNome = ft.Fornecedor.Nome,
+                                ProdutoNome = ft.Produto.Nome,
+                                Quantidade = ft.Quantidade,
+                                Valor = ft.Valor,
+
+
+
+                            };
+                return View((dados).ToList());
+            }
+           
+        }
+
+        public ActionResult Saidas()
+        {
+            return View();
+        }
+
+        public ActionResult Clientes()
+        {
+            return View();
+        }
+    }
+}
---
+++ CodingCraftHOMod1Ex1EF/ViewModel/MovimentacaoFornecedorsPesquisaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodingCraftHOMod1Ex1EF.ViewModel
+{
+    public class MovimentacaoFornecedorsPesquisaModel
+    {
+        public DateTime Data { get;  set; }
+        public char FormaPagamento { get;  set; }
+        public string FornecedorNome { get;  set; }
+        public string ProdutoNome { get;  set; }
+        public int Quantidade { get;  set; }
+        public double Valor { get;  set; }
+    }
+}
